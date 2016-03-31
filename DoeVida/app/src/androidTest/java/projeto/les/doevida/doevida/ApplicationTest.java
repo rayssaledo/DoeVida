a7ac371f1b1566284b0fdb92fc7326eb655ad456
Lucas Marlon <lucas.marques@ccc.ufcg.edu.br>
2016-03-31 13:44:44 -0300

    Criação do projeto

---
+++ DoeVida/app/src/androidTest/java/projeto/les/doevida/doevida/ApplicationTest.java
@@ -0,0 +1,13 @@
+package projeto.les.doevida.doevida;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
