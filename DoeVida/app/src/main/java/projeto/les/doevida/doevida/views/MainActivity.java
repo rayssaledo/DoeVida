963c7580a162cbf4074b5a7b4cb544813c9de89b
Lucas Marlon <lucas.marques@ccc.ufcg.edu.br>
2016-04-02 10:23:19 -0300

    Tela de cadastro criada

--- DoeVida/app/src/main/java/projeto/les/doevida/doevida/views/MainActivity.java
+++ DoeVida/app/src/main/java/projeto/les/doevida/doevida/views/MainActivity.java
@@ -1,4 +1,4 @@
-package projeto.les.doevida.doevida;
+package projeto.les.doevida.doevida.views;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import projeto.les.doevida.doevida.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ DoeVida/app/src/main/java/projeto/les/doevida/doevida/views/UserCadastreActivity.java
@@ -0,0 +1,32 @@
+package projeto.les.doevida.doevida.views;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
+
+import br.com.jansenfelipe.androidmask.MaskEditTextChangedListener;
+import projeto.les.doevida.doevida.R;
+
+public class UserCadastreActivity extends AppCompatActivity {
+
+    private EditText input_date_of_birth, input_date_of_last_donation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_cadastre);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        input_date_of_birth = (EditText) findViewById(R.id.input_date_of_birth);
+        input_date_of_last_donation = (EditText) findViewById(R.id.input_date_of_last_donation);
+
+        MaskEditTextChangedListener maskDateOfBirth = new MaskEditTextChangedListener("##/##/####", input_date_of_birth);
+        MaskEditTextChangedListener maskDateOfLastDonation = new MaskEditTextChangedListener("##/##/####", input_date_of_last_donation);
+
+        input_date_of_birth.addTextChangedListener(maskDateOfBirth);
+        input_date_of_last_donation.addTextChangedListener(maskDateOfLastDonation);
+    }
+
+}
