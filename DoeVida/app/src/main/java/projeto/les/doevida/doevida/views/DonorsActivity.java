69902e1bb5d07785ccabeeae35f4365755b83be7
marcosasn <marcos.nascimento@ccc.ufcg.edu.br>
2016-04-06 11:28:16 -0300

    Ajustes de DonorsAdapter e inserção de ícones em menu.

--- DoeVida/app/src/main/java/projeto/les/doevida/doevida/views/DonorsActivity.java
+++ DoeVida/app/src/main/java/projeto/les/doevida/doevida/views/DonorsActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 import projeto.les.doevida.doevida.R;
@@ -17,4 +19,10 @@ public class DonorsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_donors);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
 }
