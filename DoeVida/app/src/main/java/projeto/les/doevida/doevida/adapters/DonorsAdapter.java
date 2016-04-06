5de54c687d26018642a464be7f14cffd41fd2ab2
marcosasn <marcos.nascimento@ccc.ufcg.edu.br>
2016-04-06 10:47:10 -0300

    Criada a tela inicial de doadores.

--- DoeVida/app/src/main/java/projeto/les/doevida/doevida/adapters/DonorsAdapter.java
+++ DoeVida/app/src/main/java/projeto/les/doevida/doevida/adapters/DonorsAdapter.java
@@ -1,30 +1,63 @@
 package projeto.les.doevida.doevida.adapters;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
-import projeto.les.doevida.doevida.R;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class DonorsAdapter extends AppCompatActivity {
+import java.util.List;
+
+import projeto.les.doevida.doevida.R;
+import projeto.les.doevida.doevida.models.User;
+
+public class DonorsAdapter extends BaseAdapter {
+
+    private LayoutInflater mInflater;
+    private List<User> items;
+
+    public DonorsAdapter(Context context, List<User> items) {
+        this.items = items;
+        mInflater = LayoutInflater.from(context);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_donors_adapter);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    public int getCount() {
+        return items.size();
+    }
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    @Override
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        User item = items.get(position);
+        convertView = mInflater.inflate(R.layout.my_item_donor, null);
+
+        ((ImageView) convertView.findViewById(R.id.iv_blood_type)).setImageBitmap(null);
+        ((TextView) convertView.findViewById(R.id.tv_donor_name)).setText(item.getName());
+        ((TextView) convertView.findViewById(R.id.tv_city)).setText(item.getCity());
+        ((ImageView) convertView.findViewById(R.id.iv_number_donates)).setImageBitmap(null);
+        ((TextView) convertView.findViewById(R.id.tv_last_donation)).setText(item.getLastDonation().toString());
+
+        return convertView;
     }
 
 }
--- DoeVida/app/src/main/java/projeto/les/doevida/doevida/views/DonorsActivity.java
+++ DoeVida/app/src/main/java/projeto/les/doevida/doevida/views/DonorsActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
 import projeto.les.doevida.doevida.R;
 
 public class DonorsActivity extends AppCompatActivity {
@@ -14,17 +15,6 @@ public class DonorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donors);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
