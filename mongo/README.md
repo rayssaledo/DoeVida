2cd879fa792eeebefb2ece2f4b567c0ca3c3e59d
unknown <andreza.queiroz@ccc.ufcg.edu.br>
2016-04-09 23:54:02 -0300

    adicionando métodos: updateLastDonation, getUsersByBloodType e addForm

--- mongo/README.md
+++ mongo/README.md
@@ -1,7 +1,14 @@
 POST http://doevida-grupoles.rhcloud.com/addUser (login, pass, name, state, city, birthDate, gender, bloodType,	lastDonation, canDonate) -- cadastra um usuário
 	
 POST http://doevida-grupoles.rhcloud.com/checkLogin (login, pass) -- verifica se o usuário cujo login foi passado como parâmetro foi cadastrado
+
+POST http://doevida-grupoles.rhcloud.com/updateLastDonation (login, lastDonation) -- atualiza a data da última doação 
+
+POST http://doevida-grupoles.rhcloud.com/addForm (login, patientName, hospitalName, city, bloodType, deadline) -- adiciona um formulario ao usuário informado
 	
 GET http://doevida-grupoles.rhcloud.com/getUser (login) -- retorna o usuario cujo login foi passado como parâmetro
 
 GET http://doevida-grupoles.rhcloud.com/getAllUsers -- retorna todos os usuários cadastrados
+
+GET http://doevida-grupoles.rhcloud.com/getUsersByBloodType (bloodType) -- recupera todos os usuários pelo tipo de sangue informado
+
--- mongo/models/user.js
+++ mongo/models/user.js
@@ -42,7 +42,7 @@ module.exports = function(mongodb, app, userCollection) {
 						if(err){
 							res.json({ "ok" : 0, "msg" : err });
 						} else {
-							res.json({"ok": 1});	
+							res.json({"ok": 1, "msg": "Cadastro realizado com sucesso!"});	
 						}
 					});
 				}
@@ -113,6 +113,146 @@ module.exports = function(mongodb, app, userCollection) {
 			
 		});
 	});	
+	
+	app.post("/updateLastDonation", function(req, res) {
+		var login = req.body.login;
+		var lastDonation = req.body.lastDonation;
+		
+		if (!login) {
+			res.json({"ok": 0, "msg": "Login do usuário não informado!"});
+		} else if (!lastDonation) {		
+			res.json({"ok": 0, "msg": "Data não informada!"});
+		} else {
+			userCollection.find({
+				login: login
+				
+			}).toArray(function(err, array){
+				if(err) {
+					res.json({ "ok" : 0, "msg" : err });
+				} else {					
+					if(array.length < 1) {						
+						res.json({ "ok" : 0, "msg" : "Usuário não encontrado!" });
+					} else {
+						userCollection.update({
+							login: login
+						},
+						{ $set: {"lastDonation": lastDonation}
+						
+						},function(err, doc){
+							if(err){
+								res.json({"ok" : 0, "msg" : err });
+							} else {
+								res.json({"ok": 1, "msg": "Data atualizada!"});	
+								
+							}
+						});
+					}
+				}				
+			});
+		}		
+	});
+	
+	app.get("/getUsersByBloodType", function(req, res){		
+		var bloodType = req.query.bloodType;
+		
+		if (!bloodType) {
+			res.json({"ok": 0, "msg": "Tipo de sangue não informado!"});
+		} else {
+			userCollection.find({
+				bloodType: bloodType
+			}).toArray(function(err, array){
+				if(err) {
+					res.json({ "ok" : 0, "msg" : err });
+				} else {					
+					if(array.length < 1) {						
+						res.json({ "ok" : 0, "msg" : "Nenhum usuário com o tipo "+ bloodType +" cadastrado!" });
+					} else {
+						res.json({ "ok" : 1, "result" : array });
+					}
+				}
+				
+			});
+		}
+		
+	});	
+	
+	app.post("/addForm", function(req, res){
+		var login = req.body.login;
+		var patientName = req.body.patientName;
+		var hospitalName = req.body.hospitalName;
+		var city = req.body.city;
+		var bloodType = req.body.bloodType;
+		var deadline = req.body.deadline;
+		
+		var inconsistency = checkInconsistencyForm(login, patientName, hospitalName, city, bloodType, deadline);
+		
+		if (inconsistency) {
+			res.send(inconsistency);
+		} else {
+			userCollection.find({
+				login: login
+			}).toArray(function(err, array){
+				if(err) {
+					res.json({ "ok" : 0, "msg" : err });
+				} else {					
+					if(array.length < 1) {						
+						res.json({ "ok" : 0, "msg" : "Usuário não encontrado!"});
+					} else {
+						userCollection.update({
+							login: login
+						},
+						{ 
+							$push: { 
+								forms: {								
+									patientName: patientName, 
+									hospitalName: hospitalName, 
+									city: city, 
+									bloodType: bloodType, 
+									deadline: deadline								
+								} 
+							}	
+						},function(err, doc){
+							if(err){
+								res.json({"ok" : 0, "msg" : err });
+							} else {
+								res.json({"ok": 1, "msg": "Form Criado!"});	
+								
+							}
+						});
+					}
+				}
+				
+			});			
+		}
+	});		
+		
+	function checkInconsistencyForm(login, patientName, hospitalName, city, bloodType, deadline) {
+		if (!login) {
+			return '{ "ok" : 0, "msg" : "Login do usuário não informado!" }';
+		}
+		
+		if (!patientName) {
+			return '{ "ok" : 0, "msg" : "Nome do paciente não informado!" }';
+		}
+		
+		if (!hospitalName) {
+			return '{ "ok" : 0, "msg" : "Hospital não informado!" }';
+		}
+		
+		if (!city) {
+			return '{ "ok" : 0, "msg" : "Cidade não informada!" }';
+		}
+		
+		if (!bloodType) {
+			return '{ "ok" : 0, "msg" : "Tipo de sangue não informado!" }';
+		}
+		
+		if (!deadline) {
+			return '{ "ok" : 0, "msg" : "Data limite não informada!" }';
+		}
+		
+	}
+	
 	
 	function checkInconsistencyLogin(login, pass) {
 		if (!login) {
