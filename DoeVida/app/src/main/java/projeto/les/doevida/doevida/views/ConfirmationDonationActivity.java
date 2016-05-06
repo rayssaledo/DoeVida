a006927cb43e475fa93ef8de6218bbcaabb7079a
marcosasn <marcos.nascimento@ccc.ufcg.edu.br>
2016-05-06 13:39:51 -0300

    Criacao de tela de confirmar recebimento de doacao

---
+++ DoeVida/app/src/main/java/projeto/les/doevida/doevida/views/ConfirmationDonationActivity.java
@@ -0,0 +1,21 @@
+package projeto.les.doevida.doevida.views;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import projeto.les.doevida.doevida.R;
+
+public class ConfirmationDonationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmation_donation);
+
+    }
+
+}
